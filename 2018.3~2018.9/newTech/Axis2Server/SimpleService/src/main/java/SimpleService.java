7b3ecacb1c5720ea3566dc00d609acfdb90be97b
pwhpwhst <pwhpwhst@163.com>
2018-11-02 04:10:59 +0800

    Axis2服务搭建样本

---
+++ 2018.3~2018.9/newTech/Axis2Server/SimpleService/src/main/java/SimpleService.java
@@ -0,0 +1,11 @@
+
+public class SimpleService{
+	public String getGreeting(String name){
+		return "ÄãºÃ£º"+name;
+	}
+
+	public int getPrice(String name){
+		return 1000;
+	}
+
+}
---
+++ 2018.3~2018.9/newTech/Axis2Server/version/src/main/java/sample/axisversion/Version.java
@@ -0,0 +1,10 @@
+package sample.axisversion;
+
+public class Version
+{
+  public String getVersion()
+    throws Exception
+  {
+    return "Hi - the Axis2 version is " + "1.1";
+  }
+}
