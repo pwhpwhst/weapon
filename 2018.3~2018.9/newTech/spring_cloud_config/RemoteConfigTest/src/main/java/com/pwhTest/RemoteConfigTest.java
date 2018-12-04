ef1bd92cb37ca9d8c7464c9ab81b98f3c6771fd2
xuesong <xuesong.pwh@dtyunxi.com>
2018-12-04 16:06:26 +0800

    实现配置热刷新

--- 2018.3~2018.9/newTech/spring_cloud_config/RemoteConfigTest/src/main/java/com/pwhTest/RemoteConfigTest.java
+++ 2018.3~2018.9/newTech/spring_cloud_config/RemoteConfigTest/src/main/java/com/pwhTest/RemoteConfigTest.java
@@ -3,14 +3,16 @@ package com.pwhTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 //@EnableDiscoveryClient
-@Configuration
-@EnableAutoConfiguration
+//@Configuration
+//@EnableAutoConfiguration
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RemoteConfigTest{
 	public static void main(String args[]){
 		SpringApplication.run(RemoteConfigTest.class, args);
--- 2018.3~2018.9/newTech/spring_cloud_config/RemoteConfigTest/src/main/java/com/pwhTest/controller/HelloWorldController.java
+++ 2018.3~2018.9/newTech/spring_cloud_config/RemoteConfigTest/src/main/java/com/pwhTest/controller/HelloWorldController.java
@@ -3,8 +3,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value; 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @RestController
+@RefreshScope
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
