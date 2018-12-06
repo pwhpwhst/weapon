64abbf55623f794e910f02f91ac10097bac58560
pwhpwhst <pwhpwhst@163.com>
2018-12-07 04:14:37 +0800

    开始研究eureka

---
+++ 2018.3~2018.9/newTech/spring-cloud-Netflix/springboot/demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+import com.netflix.discovery.EurekaClient;
+import javax.annotation.Resource;
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class HelloWorldController {
+
+@Autowired
+private EurekaClient discoveryClient;
+
+    @RequestMapping("/hello")
+    public String index() {
+
+
+    InstanceInfo instance = discoveryClient.getNextServerFromEureka("PWHPWHST", false);
+    return instance.getHomePageUrl();
+
+    }
+}
