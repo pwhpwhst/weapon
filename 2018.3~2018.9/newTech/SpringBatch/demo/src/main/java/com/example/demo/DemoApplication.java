c80122a6fdb13dcc6ad42044badc39aa6b5f96d7
pwhpwhst <pwhpwhst@163.com>
2020-02-27 23:05:18 +0800

    spring batch 样本

---
+++ 2018.3~2018.9/newTech/SpringBatch/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,133 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.example.demo.bean.Person;
+import com.example.demo.batchprocessing.PersonItemProcessor;
+import com.example.demo.batchprocessing.JobCompletionNotificationListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.SQLException;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+@Configuration
+@SpringBootApplication
+@EnableBatchProcessing
+public class DemoApplication {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	// end::setup[]
+
+	// tag::readerwriterprocessor[]
+	@Bean
+	public FlatFileItemReader<Person> reader() {
+		return new FlatFileItemReaderBuilder<Person>()
+			.name("personItemReader")
+			.resource(new ClassPathResource("sample-data.csv"))
+			.delimited()
+			.names(new String[]{"firstName", "lastName"})
+			.fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+				setTargetType(Person.class);
+			}})
+			.build();
+	}
+
+	@Bean
+	public PersonItemProcessor processor() {
+		return new PersonItemProcessor();
+	}
+
+	@Bean
+	public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Person>()
+			.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+			.sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+			.dataSource(dataSource)
+			.build();
+	}
+	// end::readerwriterprocessor[]
+
+	// tag::jobstep[]
+	@Bean
+	public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+		return jobBuilderFactory.get("importUserJob")
+			.incrementer(new RunIdIncrementer())
+			.listener(listener)
+			.flow(step1)
+			.end()
+			.build();
+	}
+
+	@Bean
+	public Step step1(JdbcBatchItemWriter<Person> writer) {
+		return stepBuilderFactory.get("step1")
+			.<Person, Person> chunk(10)
+			.reader(reader())
+			.processor(processor())
+			.writer(writer)
+			.build();
+	}
+	// end::jobstep[]
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Bean(name = "dataSource",destroyMethod = "close")
+    public DataSource dataSource() throws SQLException {
+
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("059030849");
+
+        dataSource.setInitialSize(1);
+        dataSource.setMaxActive(5);
+        dataSource.setMinIdle(0);
+        dataSource.setMaxWait(60000);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(false);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTimeBetweenEvictionRunsMillis(60000);
+        dataSource.setMinEvictableIdleTimeMillis(25200000);
+        dataSource.setRemoveAbandoned(true);
+        dataSource.setRemoveAbandonedTimeout(1800);
+        dataSource.setLogAbandoned(true);
+        dataSource.setFilters("mergeStat");
+
+        dataSource.init();
+
+        return dataSource;
+    }
+}
+
+
+
