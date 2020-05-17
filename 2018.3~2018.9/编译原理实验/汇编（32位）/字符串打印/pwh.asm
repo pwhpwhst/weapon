722502d4e91b8f68f12b023e3e7ac6a1108fbd0f
pwhpwhst <pwhpwhst@163.com>
2020-05-18 01:02:54 +0800

    更新

---
+++ 2018.3~2018.9/编译原理实验/汇编（32位）/字符串打印/pwh.asm
@@ -0,0 +1,17 @@
+DATA SECTION
+;
+KEEP DD 0               ;temporary place to keep things
+;
+CODE SECTION
+;
+START:
+PUSH -11                ;STD_OUTPUT_HANDLE
+CALL GetStdHandle       ;get, in eax, handle to active screen buffer
+PUSH 0
+PUSH ADDR KEEP        ;KEEP receives output from API
+PUSH 24
+PUSH 'Hello World (from GoAsm)'    ;24=length of string
+PUSH EAX                ;handle to active screen buffer
+CALL WriteFile
+XOR EAX,EAX             ;return eax=0 as preferred by Windows
+RET
