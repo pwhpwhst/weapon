8ce2140b97a1936fe3e27f9cf37f36e1f21cdc6c
pwhpwhst <pwhpwhst@163.com>
2020-11-08 23:05:11 +0800

    Lexical模块实现中

--- 2018.3~2018.9/编译原理实验/微型编译器/pwh.cpp
+++ 2018.3~2018.9/编译原理实验/微型编译器/pwh.cpp
@@ -2,17 +2,16 @@
 #include"symbols\CompileInfo.h"
 #include"slr.h"
 #include"symbols\Type.h"
+#include"SLR\Lex_Word.h"
 
-
-void test3();
-int main(){
-test3();
-}
+#include <vector>
 
 
 
 
-void test3(){
+
+
+void test1(){
 string rule_file="D:\\Users\\Administrator\\Desktop\\project2018.3_2018.9\\2018.3~2018.9\\编译原理实验\\微型编译器\\pwh的试验\\rule.txt";
 string compile_file="D:\\Users\\Administrator\\Desktop\\project2018.3_2018.9\\2018.3~2018.9\\编译原理实验\\微型编译器\\pwh的试验\\test.txt";
 //string rule_file="F:\\codeWeaponStore\\project2018.3_2018.9\\2018.3~2018.9\\编译原理实验\\微型编译器\\rule.txt";
@@ -43,4 +42,33 @@ cout<<t1->content<<endl;
 
 cout<<"检测完成！"<<endl;
 
+}
+
+
+
+
+void test2(){
+
+string compile_file="D:\\Users\\Administrator\\Desktop\\project2018.3_2018.9\\2018.3~2018.9\\编译原理实验\\微型编译器\\pwh的试验\\testForLexical.txt";
+
+
+
+
+vector<P_Lex_Word>  total_lex_word_list;
+total_lex_word_list.clear();
+word_parser(compile_file,total_lex_word_list);
+
+for(const auto &e:total_lex_word_list){
+	cout<<"type="<<e->type<<endl;
+}
+
+
+}
+
+
+
+
+int main(){
+//test1();
+test2();
 }
