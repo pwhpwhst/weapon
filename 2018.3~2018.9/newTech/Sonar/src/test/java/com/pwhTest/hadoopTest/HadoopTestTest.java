554e2e0de508ace44bd601fe677d5a2e26f41cc7
pwhpwhst <pwhpwhst@163.com>
2021-03-15 00:32:02 +0800

    sonar研究

---
+++ 2018.3~2018.9/newTech/Sonar/src/test/java/com/pwhTest/hadoopTest/HadoopTestTest.java
@@ -0,0 +1,15 @@
+package com.pwhTest.hadoopTest;
+import org.junit.Test;
+public class HadoopTestTest{
+
+  @Test
+  public void testHelloJunit() {
+	  String[] args={};
+	  HadoopTest.main(args);
+//      System.out.println("fdsgdsgdsg");
+  }
+}
+
+
+
+
